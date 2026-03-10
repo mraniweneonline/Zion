6cb40d7579e62046c26dd6dac517d1528002c9b7
Aniwene Madolomani <aniwenemadolomani@gmail.com>
2026-03-10 09:29:31 +0300

    Changed menu. Exit is now 0 to allow more menu items, and not limited to 10 which was exit before.

--- Software/Core/ZionCore/Program.cs
+++ Software/Core/ZionCore/Program.cs
@@ -7,8 +7,7 @@ class Program
         Console.WriteLine("==============================================");
         Console.WriteLine("Welcome to ZionCore - v2026.0310.0001.0001");
         Console.WriteLine("===============================================");
-        Console.WriteLine("[1] Home [2] List [3] Add [4] Remove [5] Search");
-        Console.WriteLine("[*] Home [*] List [*] Add [*] Remove [10] Exit");
+        Console.WriteLine("[1] Home [0] Exit");
         Console.WriteLine("-----------------------------------------------");
     }
 }
